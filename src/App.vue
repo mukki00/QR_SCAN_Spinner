130c00cc2883df73f2e891452275a175ecf72030
mukki00 <mohamedmukkicolombo@gmail.com>
2022-04-26 20:51:11 +0530

    New axios restful function added

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,129 @@
+<!-- @format -->
+
 <template>
-  <h1 class="center">Spinner Demo</h1>
-  <h4 class="center">Status</h4>
-  <div class="center">
-    <PulseLoader :loading="loadingState" :color="color" :size="size" />
-  </div>
-  <div>
-    <p class="center">
-      <span>Status</span><span>{{ ` ` + loadingState }}</span>
-    </p>
-  </div>
-  <div>
-    <p class="center">
-      <button @click="setLoadingState">
-        {{ loadingState ? "Set to False" : "Set to True" }}
-      </button>
-    </p>
-  </div>
+	<h1 class="center">
+		Spinner Demo
+	</h1>
+	<h4 class="center">
+		Status
+	</h4>
+	<div class="center">
+		<PulseLoader
+			:loading="loadingState"
+			:color="color"
+			:size="size"
+		/>
+	</div>
+	<div>
+		<p class="center">
+			<span>Status</span
+			><span>{{
+				` ` + loadingState
+			}}</span>
+		</p>
+	</div>
+	<div>
+		<p class="center">
+			<button
+				@click="
+					setLoadingState
+				"
+			>
+				{{
+					loadingState
+						? 'Set to False'
+						: 'Set to True'
+				}}
+			</button>
+		</p>
+	</div>
+	<div class="center">
+		<button
+			@click="
+				generateRandomNum
+			"
+		>
+			Generate Rand
+		</button>
+	</div>
+	<div class="center">
+		<div
+			v-show="
+				!numberGenerated
+			"
+		>
+			<h4>
+				{{
+					randNumber === 0
+						? 'Not Generated'
+						: randNumber
+				}}
+			</h4>
+		</div>
+		<DotLoader
+			:loading="
+				numberGenerated
+			"
+			:color="newColor"
+			:size="newSize"
+		/>
+	</div>
 </template>
 <script>
-import { mapState, mapActions } from "vuex";
-import PulseLoader from "vue-spinner/src/PulseLoader.vue";
-export default {
-  name: "App",
-  data() {
-    return {
-      color: "#ff0000",
-      size: "50px",
-    };
-  },
-  computed: {
-    ...mapState("spinner", ["loadingState"]),
-  },
-  methods: {
-    ...mapActions("spinner", ["setLoadingStateTrue", "setLoadingStateFalse"]),
-    setLoadingState() {
-      if (this.loadingState) {
-        this.setLoadingStateFalse();
-      } else {
-        this.setLoadingStateTrue();
-      }
-    },
-  },
-  components: {
-    PulseLoader,
-  },
-};
+	import {
+		mapState,
+		mapActions,
+	} from 'vuex';
+	import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
+	import DotLoader from 'vue-spinner/src/DotLoader.vue';
+	export default {
+		name: 'App',
+		data() {
+			return {
+				color: '#ff0000',
+				size: '50px',
+				newColor: 'lightblue',
+				newSize: '200px',
+			};
+		},
+		computed: {
+			...mapState('spinner', [
+				'loadingState',
+				'randNumber',
+				'numberGenerated',
+			]),
+		},
+		methods: {
+			...mapActions(
+				'spinner',
+				[
+					'setLoadingStateTrue',
+					'setLoadingStateFalse',
+					'generateRandomNum',
+				],
+			),
+			setLoadingState() {
+				if (
+					this.loadingState
+				) {
+					this.setLoadingStateFalse();
+				} else {
+					this.setLoadingStateTrue();
+				}
+			},
+		},
+		components: {
+			PulseLoader,
+			DotLoader,
+		},
+	};
 </script>
 <style>
-.center {
-  text-align: center;
-}
+	.center {
+		text-align: center;
+	}
+	.v-dot {
+		margin-left: auto;
+		margin-right: auto;
+	}
 </style>
--- src/store/modules/spinner.js
+++ src/store/modules/spinner.js
@@ -1,24 +1,73 @@
+/** @format */
+
+import axios from 'axios';
 export default {
-  namespaced: true,
-  state() {
-    return {
-      loadingState: true,
-    };
-  },
-  mutations: {
-    setLoadingStateTrue(state) {
-      state.loadingState = true;
-    },
-    setLoadingStateFalse(state) {
-      state.loadingState = false;
-    },
-  },
-  actions: {
-    setLoadingStateTrue({ commit }) {
-      commit("setLoadingStateTrue");
-    },
-    setLoadingStateFalse({ commit }) {
-      commit("setLoadingStateFalse");
-    },
-  },
+	namespaced: true,
+	state() {
+		return {
+			loadingState: true,
+			randNumber: 0,
+			numberGenerated: false,
+		};
+	},
+	mutations: {
+		setLoadingStateTrue(
+			state,
+		) {
+			state.loadingState = true;
+		},
+		setLoadingStateFalse(
+			state,
+		) {
+			state.loadingState = false;
+		},
+		setNumberGeneratedTrue(
+			state,
+		) {
+			state.numberGenerated = true;
+		},
+		setRandNumber(
+			state,
+			randNumber,
+		) {
+			state.numberGenerated = false;
+			state.randNumber =
+				randNumber;
+		},
+	},
+	actions: {
+		setLoadingStateTrue({
+			commit,
+		}) {
+			commit(
+				'setLoadingStateTrue',
+			);
+		},
+		setLoadingStateFalse({
+			commit,
+		}) {
+			commit(
+				'setLoadingStateFalse',
+			);
+		},
+		async generateRandomNum({
+			commit,
+		}) {
+			commit(
+				'setNumberGeneratedTrue',
+			);
+			const response =
+				await axios.get(
+					'https://www.randomnumberapi.com/api/v1.0/random?min=1&max=10&count=1',
+				);
+			const randNum =
+				parseInt(
+					response.data[0],
+				);
+			commit(
+				'setRandNumber',
+				randNum,
+			);
+		},
+	},
 };
